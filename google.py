ce7401f12e5605e89631d5c236811746a5f7622a
antma <antma@users.noreply.github.com>
2024-07-19 01:23:45 +0300

    add jsa(atom) and shogidb2(rss)

--- google.py
+++ google.py
@@ -2,13 +2,14 @@
 """ download youtube playlist, google translate """
 
 #standard
+import random
 import logging
 import os
-import pickle
+import pprint
 import subprocess
 import csv
+import time
 from datetime import datetime
-from time import mktime
 
 #library
 import feedparser
@@ -66,36 +67,41 @@ class CachedCSVGoogleTranslate:
     self._updates.append((text, t))
     self._cache[text] = t
     return t
-
-def _google_translate(text):
-  """ use Google Translate via translate-shell package """
-  cmd = ['trans', '-b', '-e', 'google', 'ja:en', text]
-  logging.debug(f'Run command: {cmd}')
-  r = subprocess.run(cmd, check = False, shell = False, capture_output=True)
-  if r.returncode != 0:
-    logging.warning(f"trans exit code {r.returncode}")
-    logging.debug('%s', r.stderr.decode('utf-8'))
-    return None
-  return r.stdout.decode('utf-8')
-
-def _cvt_date(s):
-  return datetime.fromtimestamp(mktime(s))
-
-def youtube_translate_video_playlist(channel, output_rss_filename, filter_title = None):
-  rss_url = f'https://www.youtube.com/feeds/videos.xml?channel_id={channel}'
-  d = feedparser.parse(rss_url)
-  import pprint
-  logging.debug(pprint.pp(d))
-  items = []
-  f = d['feed']
-  with CachedCSVGoogleTranslate() as gt:
-    feed_title = gt.translate(f['title'])
+  def shogidb2_translate_games(self, output_rss_filename):
+    rss_url = 'https://shogidb2.com/rss'
+    d = feedparser.parse(rss_url)
+    logging.debug(pprint.pformat(d))
+    items = []
+    f = d['feed']
+    feed_title = self.translate(f['title'])
+    for e in d['entries']:
+      link = e['link']
+      if not link.startswith('https://shogidb2.com/games/'): continue
+      summary = self.translate(e['summary'])
+      title = self.translate(e['title'])
+      items.append(PyRSS2Gen.RSSItem(
+        title = title,
+        description = summary,
+        link = link,
+        guid = PyRSS2Gen.Guid(link),
+        pubDate = _cvt_date(e['published_parsed'])
+      ))
+    rss = PyRSS2Gen.RSS2(title = feed_title, description = '', link = f['link'], items = items)
+    with open(output_rss_filename, "w") as g:
+      rss.write_xml(g)
+  def youtube_translate_video_playlist(self, channel, output_rss_filename, filter_title = None):
+    rss_url = f'https://www.youtube.com/feeds/videos.xml?channel_id={channel}'
+    d = feedparser.parse(rss_url)
+    logging.debug(pprint.pformat(d))
+    items = []
+    f = d['feed']
+    feed_title = self.translate(f['title'])
     for e in d['entries']:
       title = e['title']
       views = int(e['media_statistics']['views'])
       if views == 0: continue
       if (not filter_title is None) and (not filter_title(title)): continue
-      title = gt.translate(title)
+      title = self.translate(title)
       link = e['link']
       items.append(PyRSS2Gen.RSSItem(
         title = title,
@@ -104,7 +110,43 @@ def youtube_translate_video_playlist(channel, output_rss_filename, filter_title 
         guid = PyRSS2Gen.Guid(link),
         pubDate = _cvt_date(e['published_parsed'])
       ))
-  rss = PyRSS2Gen.RSS2(title = feed_title, description = '', link = f['link'], items = items, pubDate = _cvt_date(f['published_parsed']))
-  with open(output_rss_filename, "w") as g:
-    rss.write_xml(g)
+    rss = PyRSS2Gen.RSS2(title = feed_title, description = '', link = f['link'], items = items, pubDate = _cvt_date(f['published_parsed']))
+    with open(output_rss_filename, "w") as g:
+      rss.write_xml(g)
+  def jsa_atom(self, output_rss_filename):
+    atom_url = 'https://www.shogi.or.jp/atom.xml'
+    d = feedparser.parse(atom_url)
+    logging.debug(pprint.pformat(d))
+    items = []
+    f = d['feed']
+    feed_title = self.translate(f['title'])
+    for e in d['entries']:
+      link = e['link']
+      #if not link.startswith('https://shogidb2.com/games/'): continue
+      summary = self.translate(e['summary'])
+      title = self.translate(e['title'])
+      items.append(PyRSS2Gen.RSSItem(
+        title = title,
+        description = summary,
+        link = link,
+        guid = PyRSS2Gen.Guid(link),
+        pubDate = _cvt_date(e['published_parsed'])
+      ))
+    rss = PyRSS2Gen.RSS2(title = feed_title, description = '', link = f['link'], items = items)
+    with open(output_rss_filename, "w") as g:
+      rss.write_xml(g)
+def _google_translate(text):
+  """ use Google Translate via translate-shell package """
+  cmd = ['trans', '-b', '-e', 'google', 'ja:en', text]
+  logging.debug(f'Run command: {cmd}')
+  r = subprocess.run(cmd, check = False, shell = False, capture_output=True)
+  time.sleep(random.uniform(0.5, 1.5))
+  if r.returncode != 0:
+    logging.warning(f"trans exit code {r.returncode}")
+    logging.debug('%s', r.stderr.decode('utf-8'))
+    return None
+  return r.stdout.decode('utf-8')
+
+def _cvt_date(s):
+  return datetime.fromtimestamp(time.mktime(s))
 
